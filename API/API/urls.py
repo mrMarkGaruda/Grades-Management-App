aaf81e35f0a9738c5e5c1170ca93460aebf450ee
ZaZenZe <desayan2014@gmail.com>
2025-01-27 16:53:10 +0100

    Minor changes

--- API/API/urls.py
+++ API/API/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('student_management.urls')),  # Include API routes
+    path('api/', include('student_management.urls')),
 ]
